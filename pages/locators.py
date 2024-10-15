5727f76a4f899c79fd729d36f7a1d70dc53e218b
theDeadInside228 <a320859@gmail.com>
2024-10-15 21:27:24 +0500

    добавил переход на страницу логина

--- pages/locators.py
+++ pages/locators.py
@@ -4,10 +4,6 @@ class MainPageLocators:
     
 class LoginPageLocators:
     LOGIN_FORM = (By.ID, 'login_form')
-    LOGIN_USERNAME = (By.ID, 'id_login-username')
-    LOGIN_PASSWORD = (By.ID, 'id_login-password')
     REGIST_FORM = (By.ID, 'register_form')
-    REGIST_EMAIL = (By.ID, 'id_registration-email')
-    REGIST_PASSWORD1 = (By.ID, 'id_registration-password1')
-    REGIST_PASSWORD2 = (By.ID, 'id_registration-password2')
+   
     
