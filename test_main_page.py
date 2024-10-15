13ed8c31a4ad8fd1707fce981634d534c59d7e9b
theDeadInside228 <a320859@gmail.com>
2024-10-15 16:26:30 +0500

    создал несколько классов

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,6 @@
-from selenium.webdriver.common.by import By
-import time
+from .pages.main_page import MainPage
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    time.sleep(5)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(url='http://selenium1py.pythonanywhere.com/', browser=browser)
+    page.open()
+    page.go_to_login_page()
